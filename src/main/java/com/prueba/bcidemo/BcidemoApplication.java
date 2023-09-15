b9a63541cee7eb072ffeceaa9a1c16e91ce8193e
mileonespino <mijailaldoveach.leonespino@everis.nttdata.com>
2023-09-14 23:20:31 -0500

    Creacion de proyecto inicial

---
+++ src/main/java/com/prueba/bcidemo/BcidemoApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.bcidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcidemoApplication.class, args);
+	}
+
+}
